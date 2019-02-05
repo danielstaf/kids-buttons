4eff701c905097908fe88734fbc521a1a7ec726e
Daniel Staf <daniel.staf@gmail.com>
2019-02-05 08:59:18 +0100

    Please enter the commit message for your changes. Lines starting

--- example.py
+++ example.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
+import os, sys, time
+
+streamdeck_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                              "python-elgato-streamdeck/src")
+print(streamdeck_dir)
+sys.path.append(streamdeck_dir)
+
 import StreamDeck.StreamDeck as StreamDeck
-import threading
+
 from PIL import Image, ImageDraw, ImageFont
 
 
@@ -56,70 +63,80 @@ class Key:
       self.set_color((0, 0, 0))
 
 
-def get_key_image(deck, key, state):
-   # Get the required key image dimensions
-   image_format = deck.key_image_format()
-   width = image_format['width']
-   height = image_format['height']
-   order = image_format['order']
-
-   # Create new key image of the correct dimensions, black background
-   image = Image.new("RGB", (width, height), 'black')
-
-   image.paste( (255,0,0), [0,0,image.size[0],image.size[1]])
-
-   # Load a custom TrueType font and use it to overlay the key index, draw key
-   # number onto the image
-   font = ImageFont.truetype("python-elgato-streamdeck/src/Assets/Roboto-Regular.ttf", 14)
-   draw = ImageDraw.Draw(image)
-   draw.text((10, height - 20), text="Key {}".format(key), font=font, fill=(255, 255, 255, 128))
-
-   # Get the raw r, g and b components of the generated image (note we need to
-   # flip it horizontally to match the format the StreamDeck expects)
-   r, g, b = image.transpose(Image.FLIP_LEFT_RIGHT).split()
-
-   # Recombine the B, G and R elements in the order the display expects them,
-   # and convert the resulting image to a sequence of bytes
-   rgb = { "R": r, "G": g, "B": b }
-   return Image.merge("RGB", (rgb[order[0]], rgb[order[1]], rgb[order[2]])).tobytes()
-
-
-def key_change_callback(deck, key, state):
-   print("Deck {} Key {} = {}".format(deck.id(), key, state))
+# def get_key_image(deck, key, state):
+#    # Get the required key image dimensions
+#    image_format = deck.key_image_format()
+#    width = image_format['width']
+#    height = image_format['height']
+#    order = image_format['order']
+# 
+#    # Create new key image of the correct dimensions, black background
+#    image = Image.new("RGB", (width, height), 'black')
+# 
+#    image.paste( (255,0,0), [0,0,image.size[0],image.size[1]])
+# 
+#    # Load a custom TrueType font and use it to overlay the key index, draw key
+#    # number onto the image
+#    font = ImageFont.truetype("python-elgato-streamdeck/src/Assets/Roboto-Regular.ttf", 14)
+#    draw = ImageDraw.Draw(image)
+#    draw.text((10, height - 20), text="Key {}".format(key), font=font, fill=(255, 255, 255, 128))
+# 
+#    # Get the raw r, g and b components of the generated image (note we need to
+#    # flip it horizontally to match the format the StreamDeck expects)
+#    r, g, b = image.transpose(Image.FLIP_LEFT_RIGHT).split()
+# 
+#    # Recombine the B, G and R elements in the order the display expects them,
+#    # and convert the resulting image to a sequence of bytes
+#    rgb = { "R": r, "G": g, "B": b }
+#    return Image.merge("RGB", (rgb[order[0]], rgb[order[1]], rgb[order[2]])).tobytes()
 
 
-   if key == deck.key_count() - 1:
-      deck.reset()
-      deck.close()
 
 
-if __name__ == "__main__":
+
+def main():   
+
+      
+
+   
    manager = StreamDeck.DeviceManager()
    decks = manager.enumerate()
+   
+   if len(decks) > 1:
+      sys.exit('Found more than one connected streamdeck.')
+   print("Connected to Stream Deck.")
+   deck = decks[0]
+   deck.open()
+   deck.reset()
+   deck.set_brightness(30)
 
-   print("Found {} Stream Decks.".format(len(decks)))
+   keys = [Key(deck, key_index) for key_index in range(deck.key_count())]
 
-   for deck in decks:
-      deck.open()
-      deck.reset()
 
-      print("Press key {} to exit.".format(deck.key_count() - 1))
 
-      deck.set_brightness(30)
+   def key_change_callback(deck, key, state):
+      print("Key {} = {}".format(key, state))
+   
+      keys[key].set_color((255,0,255))
+      keys[key].set_text("!")
+   
+      if key == deck.key_count() - 1:
+         deck.reset()
+         deck.close()
+         sys.exit()
 
-      keys = [Key(deck, key_index) for key_index in range(deck.key_count())]
+   deck.set_key_callback(key_change_callback)
 
-      keys[2].set_color((0,0,255))
-      keys[2].set_text("tomten")
+   while True:
+      
 
-      keys[3].set_color((255,0,0))
-      keys[3].set_text("är röd")
+#       for key in keys:
+#          key.set_color((0,0,255))
+#          key.set_text("tomten")
+#          time.sleep(0.05)
 
-      deck.set_key_callback(key_change_callback)
 
-      for t in threading.enumerate():
-         if t is threading.currentThread():
-            continue
-
-         if t.is_alive():
-            t.join()
+      time.sleep(1)
+      
+if __name__ == "__main__":
+   main()
